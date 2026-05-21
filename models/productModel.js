8f4d0e4fef137a0cd7f3ba3b21c1afd303bd2d33
Dangana Joshua <joshuatinoo33@gmail.com>
2026-05-21 03:42:42 +0100

    feat: create new model productModel

---
+++ models/productModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+	phone: String,
+	description: String,
+	images: Array, 
+	orderCode: {
+		type: String,
+		unique: true
+	}
+}, {timestamps: true});
+
+module.exports = mongoose.model('Product', productSchema);
