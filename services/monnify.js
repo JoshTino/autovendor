cedec991394fa2c2561c18f9595546aa513f8f73
Dangana Joshua <joshuatinoo33@gmail.com>
2026-07-02 19:24:12 +0100

    feat: add monnify services(API endpoint)

---
+++ services/monnify.js
@@ -0,0 +1,66 @@
+//MONNIFY ACCESS TOKEN GENERATION CODE
+const generateMonnifyAccessToken = async () => {
+
+	const response = await fetch('https://sandbox.monnify.com/api/v1/auth/login', {
+		method: 'POST',
+		headers: {
+		Authorization: 'Basic TUtfVEVTVF9HQzNCOFhHMlhYOkE2NjNOUlpBNTQ0RERQRU03S0RON1o4SFJWNllYRDhT'
+		}
+	});
+
+	const result = await response.json();
+	console.log(result);
+
+}
+
+
+const generateMonnifyDynamicAccountNumber = async (amount) => {
+
+	//MONNIFY TRANSACTION INITIALIZATION
+	const initializationResponse = await fetch('https://sandbox.monnify.com/api/v1/merchant/transactions/init-transaction', {
+	  method: 'POST',
+	  headers: {
+	    Authorization: `Bearer ${process.env.MONNIFY_ACCESS_TOKEN}`,
+	    'Content-Type': 'application/json',
+	  },
+	  body: JSON.stringify({
+	    amount: `${amount}`,
+	    customerEmail: 'stephen@ikhane.com',
+	    paymentReference: `REF_${Date.now()}`,
+	    paymentDescription: 'Trial transaction',
+	    currencyCode: 'NGN',
+	    contractCode: '5867418298',
+	    redirectUrl: 'https://my-merchants-page.com/transaction/confirm',
+	    paymentMethods: ['CARD',   'ACCOUNT_TRANSFER',   'USSD',   'PHONE_NUMBER'],
+	    metadata: {
+	      name: 'John Doe',
+	      age: 45
+	    }
+	  })
+	});
+
+	const initializationData = await initializationResponse.json();
+	//console.log(initializationData);
+
+	//GENERATE DYNAMIC ACCOUNT NUMBER
+	const response = await fetch('https://sandbox.monnify.com/api/v1/merchant/bank-transfer/init-payment', {
+		  method: 'POST',
+		  headers: {
+		    Authorization: `Bearer ${process.env.MONNIFY_ACCESS_TOKEN}`,
+		    'Content-Type': 'application/json'
+		  },
+		  body: JSON.stringify({
+		    transactionReference: `${initializationData.responseBody.transactionReference}`,
+		    bankCode: '058'
+		  })
+	});
+
+	const data = await response.json();
+	console.log(data);
+
+	return data;
+
+}
+
+
+module.exports = {generateMonnifyAccessToken, generateMonnifyDynamicAccountNumber}
