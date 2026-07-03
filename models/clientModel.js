0cd20c17e46d6bb9843a0d587017eed56c0f950a
Dangana Joshua <joshuatinoo33@gmail.com>
2026-07-03 22:06:50 +0100

    feat: add new paths(email, lastOrderCode) to client model & changed state enum items

--- models/clientModel.js
+++ models/clientModel.js
@@ -6,19 +6,17 @@ mongoose.connect(process.env.MONGO_URI)
 const clientSchema = new mongoose.Schema({
 	name: String,
 	phone: String,
+	email: String,
 	state: {
 		type: String,
 		enum: [
-			"menu",
-			"view_available_wigs",
-			"price_list",
-			"place_order",
-			"speak_to_a_representative",
-			"pick_color",
-			"select_quantity"
+			"add_email",
+			"confirm_payment",
+			"show_account_details"
 		],
-		default: "menu"
-	}
+		default: "add_email"
+	},
+	lastOrderCode: String
 }, {timestamps: true});
 
 module.exports = mongoose.model("Client", clientSchema);
